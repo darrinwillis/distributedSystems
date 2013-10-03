e10ea4a48b206a351a0908e364d50dcc6c498f1f
Bohan Li <bohanl@unix11.andrew.cmu.edu>
2013-10-02 20:47:26 -0400

    fixed issues

--- lab2/440Remote.java
+++ lab2/440Remote.java
@@ -1,4 +1,2 @@
 public interface 440Remote {
-    public void setRef(RemoteObjectReference r);
-    public RemoteObjectReference getRef();
 }
--- lab2/RemoteObjectReference.java
+++ lab2/RemoteObjectReference.java
@@ -5,28 +5,27 @@ import java.lang.*;
 public class RemoteObjectReference implements Serializable {
     public InetAddress adr;
     public int key;
-    public String interface;
+    public String name;
 
     public RemoteObjectReference(InetAddress i, int k, String n) {
         adr = i;
         key = k;
-        interface = n;
+        name = n;
     }
 
-    public 440Remote getStub() {
-		String stubName = interface + "stub";
-		440Remote stub;
+    public Object localise() {
+	String stubName = name + "_stub";
+	Object stub = null;
 		
-		try {
-			Class c = Class.forName(stubName);
-			stub = (440Remote) c.newInstance();
-			stub.setRef(this);
+	try {
+	    Class c = Class.forName(stubName);
+	    stub = c.newInstance();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
 		
-		return stub;
+	return stub;
     }
     
 }
