bb04fa44701d902f68d62ef6c1550d4e08962daa
dswillis <sherlock3211@yahoo.com>
2013-10-10 23:23:59 -0400

    Fixed tabbing on some files

--- lab2/src/TestClient.java
+++ lab2/src/TestClient.java
@@ -12,33 +12,33 @@ public class TestClient
     public static void main(String[] args)
     {
         PrintingObjectInterface obj1 = (PrintingObjectInterface)Communicate.lookup("printing1");
-	PrintingObjectInterface obj2 = (PrintingObjectInterface)Communicate.lookup("printing2");
-	int testsfailed = 0;
-	int ret1 = 0; 
-	int ret2 = 0;
-	System.out.println("Calling Method");
+        PrintingObjectInterface obj2 = (PrintingObjectInterface)Communicate.lookup("printing2");
+        int testsfailed = 0;
+        int ret1 = 0; 
+        int ret2 = 0;
+        System.out.println("Calling Method");
         try{
-	    int start = obj1.getCounter();
-	    for(int i = start + 1; i <= start + 100; i++) {
-		ret1 = obj1.printThis(testString2, increment);
-		ret2 = obj2.printThis(testString, increment);
-		if(ret1 != i) { testsfailed++; }
-		if(ret2 != i) { testsfailed++; }
-	    }
-	    obj1.printThisException(testString, 2*increment);;
+            int start = obj1.getCounter();
+            for(int i = start + 1; i <= start + 100; i++) {
+                ret1 = obj1.printThis(testString2, increment);
+                ret2 = obj2.printThis(testString, increment);
+                if(ret1 != i) { testsfailed++; }
+                if(ret2 != i) { testsfailed++; }
+            }
+            obj1.printThisException(testString, 2*increment);;
         } catch (Remote440Exception e)
-	    {
-		if(e.getCause().getClass() != ArithmeticException.class){
-		    testsfailed++;
-		    System.out.println("Exception not correctly thrown");
-		    e.printStackTrace();
-		} else {
-		    System.out.println("Exception correctly thrown");
-		}
-	    }
-	if(testsfailed == 0) 
-	    System.out.println("All Tests Passsed");
-	else
-	    System.out.println("Failed " + testsfailed + " tests");
+        {
+            if(e.getCause().getClass() != ArithmeticException.class){
+                testsfailed++;
+                System.out.println("Exception not correctly thrown");
+                e.printStackTrace();
+            } else {
+                System.out.println("Exception correctly thrown");
+            }
+        }
+        if(testsfailed == 0) 
+            System.out.println("All Tests Passsed");
+        else
+            System.out.println("Failed " + testsfailed + " tests");
     }
 }
--- lab2/src/TestConcurrent.java
+++ lab2/src/TestConcurrent.java
@@ -11,32 +11,32 @@ public class TestConcurrent extends Thread
     private static PrintingObjectInterface obj;
     
     public void run() {
-	try{
-	    obj.printThis(testString2, increment);
-	    obj.printThisException(testString, 2*increment);
+        try{
+            obj.printThis(testString2, increment);
+            obj.printThisException(testString, 2*increment);
         } catch (Remote440Exception e) {
-	    if(e.getCause().getClass() != ArithmeticException.class){
-		System.out.println("Exception not correctly thrown");
-		e.printStackTrace();
-	    } else {
-		System.out.println("Exception correctly thrown");
-	    }
-	}
+            if(e.getCause().getClass() != ArithmeticException.class){
+                System.out.println("Exception not correctly thrown");
+                e.printStackTrace();
+            } else {
+                System.out.println("Exception correctly thrown");
+            }
+        }
     }
 	
     public static void main(String[] args)
     {
         obj = (PrintingObjectInterface)Communicate.lookup("printing1");
-	try {
-	    int start = obj.getCounter();
-	    for(int i = 0; i < 50; i++)
-		(new TestConcurrent()).start();
-	    Thread.sleep(8000);
-	    if(obj.getCounter() == start + 50) 
-		System.out.println("All tests passed");
-	    else
-		System.out.println("Failure");
-	} catch(Exception e) {}
+        try {
+            int start = obj.getCounter();
+            for(int i = 0; i < 50; i++)
+                (new TestConcurrent()).start();
+            Thread.sleep(8000);
+            if(obj.getCounter() == start + 50) 
+                System.out.println("All tests passed");
+            else
+                System.out.println("Failure");
+        } catch(Exception e) {}
         
     }
 }
