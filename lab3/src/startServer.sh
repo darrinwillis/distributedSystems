1920fc7bb9b0707742151b8fd509268def19a48a
dswillis <sherlock3211@yahoo.com>
2013-11-05 17:14:53 -0500

    Added an administrative class to run the project

--- lab3/src/startServer.sh
+++ lab3/src/startServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Setting up master server"
-java -Djava.rmi.server.codebase=https://unix.andrew.cmu.edu/usr18/dswillis/private/15440/distributedSystems/lab1 MasterServer &
+java -Djava.rmi.server.codebase=https://unix.andrew.cmu.edu/usr18/dswillis/private/15440/distributedSystems/lab1 Monitor &
 
