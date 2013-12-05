b210af27ca519bd36e340bb4c97e618e07cda64f
dswillis <sherlock3211@yahoo.com>
2013-12-04 22:45:38 -0500

    Tweaked shell scripts

--- lab4/src/seqDNATest.sh
+++ lab4/src/seqDNATest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Generating random ints..."
+echo "Generating random DNA..."
 python RandomDNA.py > randomDNA.txt
 echo "Performing K-means clustering"
 java SequentialKMeans dna 3 0  randomDNA.txt
